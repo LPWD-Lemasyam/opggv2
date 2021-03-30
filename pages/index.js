bf9787c689c39b30f585f2dd9980f87025036cc2
SylvainSchwartzmann <49903970+SylvainSchwartzmann@users.noreply.github.com>
2021-03-30 13:16:38 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+// posts will be populated at build time by getStaticProps()
+function Blog({ posts }) {
+  console.log(posts);
+  return (
+    <ul>
+      <li>{posts.name}</li>
+      <li>{posts.summonerLevel}</li>
+    </ul>
+  )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Carbo%20IRL?api_key=RGAPI-13b1a15d-323a-471a-bae8-d6dcc248175e')
+  const posts = await res.json()
+  console.log(posts);
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default Blog

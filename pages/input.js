093746d28b82a887761b1ad9a1f73234b9e99770
SylvainSchwartzmann <49903970+SylvainSchwartzmann@users.noreply.github.com>
2021-05-10 09:30:25 +0200

    Update input.js

--- pages/input.js
+++ pages/input.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 function Input() {
   function redirection(){
     var val = document.querySelector("input").value;
-    document.location.href="http://localhost:3000/process?id="+val; 
+    document.location.href="https://opggv2.netlify.app/process?id="+val; 
 
     
   }
@@ -20,7 +20,6 @@ function Input() {
     // </Link>
     <div>
       <input type="text"></input>
-      {/* <a href="http://localhost:3000/test?id=Carbo+IRL">Bouton</a> */}
       <a onClick={redirection}>Bouton</a>
     </div>
     
